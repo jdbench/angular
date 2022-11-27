e051265bb8d622171bc14968e25cc7b1141fbc1e
jdbench <justin.d.bench@gmail.com>
2022-11-27 00:34:28 -0700

    Week 11 wip

---
+++ server/model/sequence.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const sequenceSchema = mongoose.Schema({
+  maxDocumentId: {
+    type: Number,
+    required: true,
+  },
+  maxMessageId: { type: Number, required: true },
+  maxContactId: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Sequence", sequenceSchema);
